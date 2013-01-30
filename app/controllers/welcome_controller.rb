c5a80f339a4cacd58e23642712370047362482e5
Damian <d.chan@2009.ljmu.ac.uk>
2013-01-30 21:39:49 +0000

    OSC
    New OSC Rails App

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+  def aboutus
+  end
+
+  def contactus
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Osc::Application.config.secret_token = '6da1b78d20f9a52a262f31bf5b73187e83a36287acce46343946ef4b6c21a0ca427b3a6a2e3f42cf6c9079743c70a54df25319cf8e8434d15e8e87c33f8e3a8b'
