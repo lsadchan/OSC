36357c341574a995f081ec110157605e2dbfe986
Damian <d.chan@2009.ljmu.ac.uk>
2013-02-05 15:52:49 +0000

    Sessions Update.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    @sessions = Session.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @sessions }
+    end
   end
   
   def learntoski
